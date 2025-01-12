8068c0285055925169fa073a908c903c8b4611be
dione <dlamericaan@gmail.com>
2025-01-12 20:55:47 +0100

    yippie

--- WDPR_2024/client/src/pages/BOVerhuurAanvragenPage.js
+++ WDPR_2024/client/src/pages/BOVerhuurAanvragenPage.js
@@ -1,7 +1,7 @@
 ﻿import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import "./VerhuurAanvragenPage.css";
+import "./BOVerhuurAanvragenPage.css";
 
 const VerhuurAanvragenPage = () => {
     const [aanvragen, setAanvragen] = useState([]);
